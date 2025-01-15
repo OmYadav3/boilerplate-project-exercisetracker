d6dd76e5fbdc2c1b6a39906c9ddd89d9060fac6d
OmYadav <omydv3@gmail.com>
2025-01-15 21:27:48 +0530

    Make the endpoint to get the users

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+const users = [];
+app.post("/api/users", (req, res) => {
+  //TODO: create the user object
+  const { username } = req.body;
+  if (!username) {
+    return res
+     .status(400)
+     .json({ error: "username are required" });
+  }
+  const newUser = { id: Date.now().toString(), username, exercises: [] };
+  users.push(newUser);
+  res.json(newUser);
 
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
